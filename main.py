9ac5e363c1e06d153dd3b68226aef0c05168aff8
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2021-12-03 13:09:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import requests
 
 engine=pyttsx3.init()
 voices=engine.getProperty('voices')
-engine.setProperty('voice','voices[1].id')
+engine.setProperty('voice','voices[0].id')
 
 def speak(text):
     engine.say(text)
